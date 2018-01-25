a9a16fda614860085223e92136be56fcb34ba520
Nathan Musoke <nathan.musoke@gmail.com>
2018-01-25 14:26:03 +1300

    DeviceId -> newtype struct

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -9,11 +9,11 @@ pub enum Error {
 }
 
 #[derive(Debug, Clone)]
-pub struct DeviceId {
+pub struct DeviceId(
     // TODO: Any requirements on format? Spec just says string; most examples seem to be ~10 upper
     // case letters
-    id: String,
-}
+    String,
+);
 
 impl DeviceId {
     pub fn new() -> DeviceId {
@@ -32,7 +32,7 @@ impl DeviceId {
 
 impl fmt::Display for DeviceId {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.id)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -44,7 +44,7 @@ impl<'a> TryFrom<&'a str> for DeviceId {
     // presumably there is some length limit.
     fn try_from(s: &'a str) -> Result<DeviceId, Error> {
         if s.is_ascii_uppercase() {
-            Ok(DeviceId { id: s.into() })
+            Ok(DeviceId(s.into()))
         } else {
             Err(Error::InvalidCharacters)
         }
@@ -68,6 +68,6 @@ mod test {
     fn generate() {
         let device_id = DeviceId::new();
 
-        assert!(device_id.id.is_ascii_uppercase());
+        assert!(device_id.0.is_ascii_uppercase());
     }
 }
